fd8d60971cd1c962d6576cc79021d0b5cc006994
joneskyle0 <joneskyle0@gmail.com>
2023-10-29 12:02:03 -0400

    list of topics

--- upcoming_topics.js
+++ upcoming_topics.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const topicsNumber = parseInt(firstRow.querySelector("td:nth-child(2)").textContent.match(/\d+/)[0]);
 
     // Create a message for the upcoming topics
-    const message = `Next week's topic is ${nextDate}: Topics ${topicsNumber + 1}`;
+    const message = `Next week's topic is ${nextDate}: Topic ${topicsNumber + 1}`;
 
     // Display the message above the table with larger size and centered
     const upcomingTopicsElement = document.getElementById("upcoming-topics");
