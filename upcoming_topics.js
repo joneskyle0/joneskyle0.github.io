cd1e5aa5cc3bdba56a7a75f6c09abaaf3109027d
joneskyle0 <joneskyle0@gmail.com>
2023-10-29 11:48:08 -0400

    Update upcoming_topics.js

--- upcoming_topics.js
+++ upcoming_topics.js
@@ -1,13 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Find the first row of the table (skipping the header row)
-    const firstRow = document.querySelector(".upcoming-topics tbody tr:first-child");
+    // Find the current date
+    const currentDate = new Date();
 
-    // Get the date and topic from the first row
-    const date = firstRow.querySelector("td:first-child").textContent;
-    const topic = firstRow.querySelector("td:nth-child(2)").textContent;
+    // Calculate the next date (one week later)
+    currentDate.setDate(currentDate.getDate() + 7);
+
+    // Format the next date as "MM/DD/YYYY"
+    const nextDate = `${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()}`;
+
+    // Get the topic number from the first row
+    const firstRow = document.querySelector(".upcoming-topics tbody tr:first-child");
+    const topicNumber = parseInt(firstRow.querySelector("td:nth-child(2)").textContent.match(/\d+/)[0]);
 
     // Create a message for the upcoming topic
-    const message = `Next week's topic is ${date}: ${topic}`;
+    const message = `Next week's topic is ${nextDate}: Topic ${topicNumber + 1}`;
 
     // Display the message above the table
     const upcomingTopicElement = document.getElementById("upcoming-topic");
