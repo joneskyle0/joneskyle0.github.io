395114a27686e4057be8a303d7a4a3099dc61d13
joneskyle0 <joneskyle0@gmail.com>
2023-10-29 13:00:43 -0400

    js attempt

--- upcoming_events.js
+++ upcoming_events.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Initialize DataTables with sorting by the date column in descending order
-    $('#upcoming-events').DataTable({
+    const table = $('#upcoming-events').DataTable({
         "order": [[1, "desc"]] // 1 corresponds to the Date column
     });
 
     // Select elements for filtering
     const locationFilter = document.getElementById("location-filter");
     const monthFilter = document.getElementById("month-filter");
+    const nextEventRow = document.getElementById("next-event-row");
 
     // Table rows
     const rows = document.querySelectorAll(".upcoming-events tbody tr");
@@ -32,7 +33,8 @@ document.addEventListener("DOMContentLoaded", function () {
         const time = nextEvent.querySelector("td:nth-child(3)").textContent;
         const topic = nextEvent.querySelector("td:nth-child(4)").textContent;
 
-        const message = `My next event is on ${date} at ${time} and the topic(s) will be ${topic}.`;
+        const message = `My next event is on ${date} at ${location} and the topic(s) will be ${topic}.`;
+        nextEventRow.innerHTML = `<table><thead><tr><th>Location</th><th>Date</th><th>Time</th><th>Topic</th></tr></thead></table><tbody><tr><td>${location}</td><td>${date}</td><td>${time}</td><td>${topic}</td></tr></tbody>`;
         document.getElementById("upcoming-topic").textContent = message;
     }
 
