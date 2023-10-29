5e4d969daf60bb1300919cb96c64066e626c4dc6
joneskyle0 <joneskyle0@gmail.com>
2023-10-29 12:09:03 -0400

    Update upcoming_topics.js

--- upcoming_topics.js
+++ upcoming_topics.js
@@ -3,18 +3,26 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const rows = document.querySelectorAll(".upcoming-topics tbody tr");
 
+    let nextTopic = null;
+    let nextDate = null;
+
     for (let row of rows) {
         const dateString = row.querySelector("td:first-child").innerText;
         const [month, day, year] = dateString.split("/");
         const rowDate = new Date(`${year}-${month}-${day}`);
 
         if (rowDate >= currentDate) {
-            const topic = row.querySelector("td:nth-child(2)").innerText;
-            const message = `The next topic is ${topic}`;
-
-            const upcomingTopicElement = document.getElementById("upcoming-topic");
-            upcomingTopicElement.querySelector(".large-text").textContent = message;
+            nextTopic = row.querySelector("td:nth-child(2)").innerText;
+            nextDate = dateString;
             break;
         }
+    }
+
+    const upcomingTopicElement = document.getElementById("upcoming-topic");
+    if (nextTopic && nextDate) {
+        const message = `The next topic is ${nextTopic} on ${nextDate}`;
+        upcomingTopicElement.querySelector(".large-text").textContent = message;
+    } else {
+        upcomingTopicElement.querySelector(".large-text").textContent = "Stay tuned for upcoming dates!";
     }
 });
