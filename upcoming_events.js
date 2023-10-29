8a904912886870a9906ebf243c73566e43061ba3
joneskyle0 <joneskyle0@gmail.com>
2023-10-29 14:14:21 -0400

    Update upcoming_events.js

--- upcoming_events.js
+++ upcoming_events.js
@@ -6,29 +6,22 @@ document.addEventListener("DOMContentLoaded", function () {
     let nextEventTime = null;
     let nextEventTopic = null;
 
-    // Loop through the table rows to find the next event
     for (const row of table.rows) {
         const dateCell = row.cells[1];
         const locationCell = row.cells[0];
         const timeCell = row.cells[2];
         const topicCell = row.cells[3];
 
-        // Parse the date in "M/D/YYYY" format
-        const dateParts = dateCell.textContent.split("/");
-        const eventDate = new Date(`${dateParts[2]}-${dateParts[0]}-${dateParts[1]}`);
+        const eventDate = new Date(dateCell.textContent);
 
-        // Check if the event date is today or in the future
-        if (eventDate >= today) {
-            if (!nextEventDate || eventDate < nextEventDate) {
-                nextEventDate = eventDate;
-                nextEventLocation = locationCell.textContent;
-                nextEventTime = timeCell.textContent;
-                nextEventTopic = topicCell.textContent;
-            }
+        if (eventDate >= today && (!nextEventDate || eventDate < nextEventDate)) {
+            nextEventDate = eventDate;
+            nextEventLocation = locationCell.textContent;
+            nextEventTime = timeCell.textContent;
+            nextEventTopic = topicCell.textContent;
         }
     }
 
-    // Display the information above the table
     if (nextEventDate) {
         const formattedDate = nextEventDate.toLocaleDateString();
         const message = `My next event is at ${nextEventLocation} at ${nextEventTime} and the topic(s) will be ${nextEventTopic}`;
