3a80dd4a429e091f610eb83c34bb26a113cfe342
joneskyle0 <joneskyle0@gmail.com>
2023-10-29 23:24:30 -0400

    qotd

---
+++ display_questions.php
@@ -0,0 +1,64 @@
+<?php
+$host = "DESKTOP-13JO25K";
+$username = "joneskyle0";
+$password = "Vr++i8^lnr/M,f:6\*i8";
+$database = "qotd";
+
+$connection = mysqli_connect($host, $username, $password, $database);
+
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$query = "SELECT * FROM Questions";
+$result = mysqli_query($connection, $query);
+
+$questions = array();
+
+while ($row = mysqli_fetch_assoc($result)) {
+    $question = array(
+        'question_text' => $row['question_text'],
+        'answers' => array(),
+    );
+
+    $answers_query = "SELECT * FROM Answers WHERE question_id = " . $row['id'];
+    $answers_result = mysqli_query($connection, $answers_query);
+
+    while ($answer_row = mysqli_fetch_assoc($answers_result)) {
+        $question['answers'][] = array(
+            'answer_text' => $answer_row['answer_text'],
+            'is_correct' => $answer_row['is_correct'],
+        );
+    }
+
+    $questions[] = $question;
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Questions and Answers</title>
+</head>
+<body>
+    <h1>Questions of the Day</h1>
+    <ul>
+        <?php foreach ($questions as $question) { ?>
+            <li>
+                <strong><?php echo $question['question_text']; ?></strong>
+                <ul>
+                    <?php foreach ($question['answers'] as $answer) { ?>
+                        <li>
+                            <?php echo $answer['answer_text']; ?>
+                            <?php if ($answer['is_correct']) { echo "(Correct)"; }
+                            ?>
+                        </li>
+                    <?php } ?>
+                </ul>
+            </li>
+        <?php } ?>
+    </ul>
+</body>
+</html>
+<?php
+mysqli_close($connection);
+?>
--- index.php
+++ index.php
@@ -21,5 +21,25 @@
             Check out your <a href="trivia_cup.html">Ellison Trivia Cup Scores</a> or see what's topics are coming up in our <a href="upcoming_events.html">Upcoming Events</a>.
         </p>
     </div>
+    <h1>Question of the Day</h1>
+    <?php
+    // Include the PHP script to fetch the question of the day
+    include 'display_questions.php';
+
+    // Get a random question from the array of questions
+    $questionOfDay = $questions[array_rand($questions)];
+
+    // Display the Question of the Day
+    ?>
+    <h2><?php echo $questionOfDay['question_text']; ?></h2>
+    <ul>
+        <?php foreach ($questionOfDay['answers'] as $answer) { ?>
+            <li>
+                <?php echo $answer['answer_text']; ?>
+                <?php if ($answer['is_correct']) { echo " (Correct)"; }
+                ?>
+            </li>
+        <?php } ?>
+    </ul>
 </body>
 </html>
