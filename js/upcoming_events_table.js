c770c8df72e0271c8917619f806ae8b1597abfba
joneskyle0 <joneskyle0@gmail.com>
2023-12-21 21:00:30 -0500

    fix js path

--- js/upcoming_events_table.js
+++ js/upcoming_events_table.js
@@ -1,45 +1,47 @@
-$(document).ready(function () {
-    // Initialize DataTable with "datetime-moment" sorting
-    $('.upcoming-events').DataTable({
-        columnDefs: [{
-            type: 'datetime-moment',
-            targets: 0,
-            render: function (data, type, row) {
-                if (type === 'sort') {
+
+        $(document).ready(function(){
+          // Initialize DataTable with "datetime-moment" sorting
+          $('.upcoming-events').DataTable({
+            columnDefs: [
+              {
+                type: 'datetime-moment',
+                targets: 0,
+                render: function (data, type, row) {
+                  if (type === 'sort') {
                     return moment(data, 'M/D/YYYY').valueOf();
-                }
-
-                // Find the next event
-                var currentDate = moment(); // Get the current date and time
-                var tableData = $('.upcoming-events').DataTable().rows().data(); // Get the table data
-                var nextEvent = null;
-
-                for (var i = 0; i < tableData.length; i++) {
+                  }
+        
+                  // Find the next event
+                  var currentDate = moment(); // Get the current date and time
+                  var tableData = $('.upcoming-events').DataTable().data(); // Get the table data
+                  var nextEvent = null;
+        
+                  for (var i = 0; i < tableData.length; i++) {
                     var eventDate = moment(tableData[i][0], 'M/D/YYYY');
                     var eventTime = tableData[i][1];
                     var eventTopic = tableData[i][2];
-
+        
                     if (eventDate.isSameOrAfter(currentDate, 'day')) {
-                        nextEvent = {
-                            date: eventDate.format('M-D-Y'),
-                            time: eventTime,
-                            topic: eventTopic
-                        };
-                        break; // Exit the loop after finding the next event
+                      nextEvent = {
+                        date: eventDate.format('M-D-Y'),
+                        time: eventTime,
+                        topic: eventTopic
+                      };
+                      break; // Exit the loop after finding the next event
                     }
+                  }
+        
+                  if (nextEvent) {
+                    document.getElementById('event-date').textContent = nextEvent.date;
+                    document.getElementById('event-time').textContent = nextEvent.time;
+                    document.getElementById('event-topic').textContent = nextEvent.topic;
+                  }
+        
+                  return moment(data, 'M/D/YYYY').format('M-D-Y');
                 }
-
-                if (nextEvent) {
-                    $('#event-date').text(nextEvent.date);
-                    $('#event-time').text(nextEvent.time);
-                    $('#event-topic').text(nextEvent.topic);
-                }
-
-                return moment(data, 'M/D/YYYY').format('M-D-Y');
-            }
-        }],
-        searching: false
-    });
-
-    $('.dataTables_length').hide();
-});
+              }
+            ],
+            searching: false
+          });
+          $('.dataTables_length').hide();
+        });
